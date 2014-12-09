4b2895608c6355d3695ad4b8084423d31ebad16a
Mathieu Rivest <mathieu.rivest@autodesk.com>
2014-12-09 01:27:19 -0500

    Cleaned up Mac-only dependencies

--- python/tk_server/__init__.py
+++ python/tk_server/__init__.py
@@ -2,12 +2,13 @@ import os, sys
 python_path = os.path.join(os.path.dirname(__file__), "../../resources/python")
 sys.path.append(os.path.join(python_path, "common"))
 
+distributions_path = os.path.join(python_path, "distributions")
 if sys.platform.startswith("darwin"):
-    sys.path.append(os.path.join(python_path, "mac"))
+    sys.path.append(os.path.join(distributions_path, "mac"))
 elif os.name == "nt":
-    sys.path.append(os.path.join(python_path, "windows"))
+    sys.path.append(os.path.join(distributions_path, "windows"))
 elif os.name == "posix":
-    sys.path.append(os.path.join(python_path, "linux"))
+    sys.path.append(os.path.join(distributions_path, "linux"))
 
 from server import Server
 from process_manager import ProcessManager
