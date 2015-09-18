80819bb7a66b4e64477bb4466dc4a95e5a48ddf1
Jean-Francois Boismenu <jean.francois.boismenu@autodesk.com>
2015-09-18 13:14:57 -0400

    For #31056, Can't run websocket server if path is too long on Windows. This branch shortens a few folders names to avoid this bug on Windows.

--- python/tk_framework_desktopserver/__init__.py
+++ python/tk_framework_desktopserver/__init__.py
@@ -14,11 +14,11 @@ import sys
 python_path = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../resources/python"))
 sys.path.append(os.path.join(python_path, "common"))
 
-distributions_path = os.path.join(python_path, "distributions")
+distributions_path = os.path.join(python_path, "dist")
 if sys.platform.startswith("darwin"):
     sys.path.append(os.path.join(distributions_path, "mac"))
 elif os.name == "nt":
-    sys.path.append(os.path.join(distributions_path, "windows"))
+    sys.path.append(os.path.join(distributions_path, "win"))
 elif os.name == "posix":
     sys.path.append(os.path.join(distributions_path, "linux"))
 
