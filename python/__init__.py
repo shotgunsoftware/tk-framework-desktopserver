1308edb5bb917074db1d8dd3d78030faab653a1c
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-04-03 11:05:48 -0400

    removing prints

--- python/__init__.py
+++ python/__init__.py
@@ -8,6 +8,4 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-print "olla"
 from . import tk_framework_desktopserver # noqa
-print "allo"
