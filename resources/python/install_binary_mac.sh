abd81fe5ff7ba71eeb77e24da1ebbcbcab0687eb
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-08-30 09:05:14 -0400

    enumerated all packages.

--- resources/python/install_binary_mac.sh
+++ resources/python/install_binary_mac.sh
@@ -1,3 +1,4 @@
 git rm -rf bin/mac
-mkdir --parents bin/mac
+rm -rf bin/mac
 /Applications/Shotgun.app/Contents/Resources/Python/bin/pip install --target bin/mac --no-deps -r binary_requirements.txt
+touch bin/mac/zope.interface/__init__.py
--- resources/python/install_source_only.sh
+++ resources/python/install_source_only.sh
@@ -1,3 +1,3 @@
 git rm -rf source
-mkdir source
+rm -rf source
 /Applications/Shotgun.app/Contents/Resources/Python/bin/pip install --target source --no-deps -r source_only_requirements.txt
