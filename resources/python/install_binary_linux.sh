03cf16ea5d24bd51cc30e2708d3bb58631c0811e
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-08-30 16:00:40 -0400

    added copyright headers

--- resources/python/install_binary_linux.sh
+++ resources/python/install_binary_linux.sh
@@ -1,6 +1,22 @@
+# Copyright (c) 2017 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
 git rm -rf bin/linux
 rm -rf bin/linux
+
+# gcc has trouble finding out libpython2.7.so, we we're adding its folder
+# to the link library path before invoking pip.
 LDFLAGS=-L/opt/Shotgun/Python/lib /opt/Shotgun/Python/bin/python build/pip install --target bin/linux --no-deps -r binary_requirements.txt
+
+# For some reason zope is missing a top level init file when installed with
+# pip, so we're adding it.
 touch bin/linux/zope/__init__.py
 
 # Remove tests to thin out the packages
--- resources/python/install_source_only.sh
+++ resources/python/install_source_only.sh
@@ -1,13 +1,30 @@
+# Copyright (c) 2017 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
 git rm -rf source
 rm -rf source
 /Applications/Shotgun.app/Contents/Resources/Python/bin/python build/pip install --target source --no-deps -r source_only_requirements.txt
 
+# We're thinning out the packages by removing unit tests.
 rm -rf source/autobahn/test
 rm -rf source/autobahn/*/test
 
 rm -rf source/twisted/test
 rm -rf source/twisted/*/test
 rm -rf source/twisted/*/*/test
+
+rm -rf source/automat/_test
+
+rm -rf source/hyperlink/test
+
+rm -rf source/incremental/tests
 
 # In twisted.internet.unix, there is a mixin which we don't use that allows to copy file descriptors
 # into other processes, which we don't require. That module is compiled, so we'll delete it.
