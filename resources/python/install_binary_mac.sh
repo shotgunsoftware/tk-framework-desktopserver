759c9db4cae26bb7897115613ff99d715283421a
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-08-31 07:29:06 -0400

    For #41325, streamlines the 3rdparty libraries for websocket usage Libraries have been split between Python only and Python+binary libraries. Scripts to update the libraries have also been added. Closes https://github.com/shotgunsoftware/tk-framework-desktopserver/pull/44

---
+++ resources/python/install_binary_mac.sh
@@ -0,0 +1,23 @@
+# Copyright (c) 2017 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+git rm -rf bin/mac
+rm -rf bin/mac
+/Applications/Shotgun.app/Contents/Resources/Python/bin/python build/pip install --target bin/mac --no-deps -r binary_requirements.txt
+
+# For some reason zope is missing a top level init file when installed with
+# pip, so we're adding it.
+touch bin/mac/zope/__init__.py
+
+# Remove tests to thin out the packages
+rm -rf bin/mac/Crypto/SelfTest
+
+rm -rf bin/mac/zope/interface/tests
+rm -rf bin/mac/zope/interface/*/tests
---
+++ resources/python/install_binary_windows.bat
@@ -0,0 +1,25 @@
+:: Copyright (c) 2017 Shotgun Software Inc.
+::
+:: CONFIDENTIAL AND PROPRIETARY
+::
+:: This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+:: Source Code License included in this distribution package. See LICENSE.
+:: By accessing, using, copying or modifying this work you indicate your
+:: agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+:: not expressly granted therein are reserved by Shotgun Software Inc.
+
+git rm -rf bin\win
+rmdir /s /q bin\win
+
+"C:\Program Files\Shotgun\Python\python.exe" build/pip install --target bin\win --no-deps -r binary_requirements.txt
+
+# Remove tests to thin out the packages
+rmdir /s /q bin\win\Crypto\SelfTest
+
+rmdir /s /q bin\win\zope\interface\tests
+
+rmdir /s /q bin\win\zope\interface\common\tests
+
+:: For some reason zope is missing a top level init file when installed with
+:: pip, so we're adding it.
+copy nul bin\win\zope\__init__.py
