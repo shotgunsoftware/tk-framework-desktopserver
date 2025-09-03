b27eff2a96e777512e49bbc578a7f21946c96cd0
Julien Langlois <julien.langlois@autodesk.com>
2025-09-03 13:52:06 -0700

    fixup! Set PIP platforms for Linux so installed wheels works on RHEL8

--- resources/python/install_binary_linux.sh
+++ resources/python/install_binary_linux.sh
@@ -29,7 +29,7 @@ mkdir $bin_dir
 pip install --target $bin_dir --no-deps \
     --platform manylinux_2_28_x86_64 \
     --platform manylinux2014_x86_64 \
-    --only-binary=:all: \
+    --only-binary :all: \
     --requirement $requirements
 
 # For some reason zope is missing a top level init file when installed with
