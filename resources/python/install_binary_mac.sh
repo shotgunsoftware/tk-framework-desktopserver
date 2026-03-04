4f981509ca21d39179893bbbc8ea4ea3ad115d3c
Julien Langlois <julien.langlois@autodesk.com>
2026-03-04 12:58:04 -0800

    test

--- resources/python/install_binary_mac.sh
+++ resources/python/install_binary_mac.sh
@@ -9,33 +9,34 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-# Get python version
-python_major_version=$(python -c "import sys; print(sys.version_info.major)")
-python_minor_version=$(python -c "import sys; print(sys.version_info.minor)")
-python_version="$python_major_version.$python_minor_version"
-
-# Set paths
-bin_dir="bin/$python_version/mac"
-requirements="bin/$python_version/explicit_requirements.txt"
-
 # Stops the script
 set -e
 
+# Get python version
+python_major_version="$(python -c "import sys; print(sys.version_info.major)")"
+python_minor_version="$(python -c "import sys; print(sys.version_info.minor)")"
+python_version="${python_major_version}.${python_minor_version}"
+
+# Set paths
+bin_dir="bin/${python_version}/mac"
+requirements="bin/${python_version}/explicit_requirements.txt"
+
+
 # Delete current files
-rm -rf $bin_dir
-mkdir $bin_dir
+rm -rf "${bin_dir}"
+mkdir -p "${bin_dir}"
 
 # Install packages
-pip install --target $bin_dir --no-deps -r $requirements
+pip install --target "${bin_dir}" --no-deps --requirement "${requirements}"
 
 # For some reason zope is missing a top level init file when installed with
 # pip, so we're adding it.
-touch $bin_dir/zope/__init__.py
+touch "${bin_dir}/zope/__init__.py"
 
 # Remove tests to thin out the packages
-rm -rf $bin_dir/Crypto/SelfTest
-rm -rf $bin_dir/zope/interface/tests
-rm -rf $bin_dir/zope/interface/*/tests
+rm -rf "${bin_dir}/Crypto/SelfTest"
+rm -rf "${bin_dir}/zope/interface/tests"
+rm -rf "${bin_dir}/zope/interface/*/tests"
 
 # Add bin dir to repo
-git add $bin_dir
+git add "${bin_dir}"
